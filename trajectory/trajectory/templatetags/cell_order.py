91c18c25ce71b79bebfc40cde69d87224f8b833e
MasterGowen <mastergowen@gmail.com>
2015-04-16 17:20:00 +0500

    fix filters

--- trajectory/trajectory/templatetags/cell_order.py
+++ trajectory/trajectory/templatetags/cell_order.py
@@ -4,3 +4,7 @@ register = template.Library()
 @register.filter
 def sort_by(queryset, order):
     return queryset.order_by(order)
+
+@register.filter
+def dictkey(dictionary, key):
+    return dictionary[key]
